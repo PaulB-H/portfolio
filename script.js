e1f30bc4433b3f5c4fa60c96c57ce2867b42833b
PaulB-H <51964537+PaulB-H@users.noreply.github.com>
2021-10-30 00:04:28 -0400

    Setup canvas

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-window.onload = (e) => {
-  console.log("Page and dependent resources have been loaded");
-};
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
