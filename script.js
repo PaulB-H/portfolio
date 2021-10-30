d730500ea57d205aa76676214402edaeaea3a11c
PaulB-H <51964537+PaulB-H@users.noreply.github.com>
2021-10-30 00:07:56 -0400

    Add resize event for canvas

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+let initialWindowWidth = window.innerWidth;
+
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+window.addEventListener("resize", () => {
+  // Check if window actually resized
+  // On mobile scrolling can trigger this event
+
+  if (initialWindowWidth !== window.innerWidth) {
+    // console.log("resize triggered, resizing canvas");
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    initialWindowWidth = window.innerWidth;
+  }
+});
