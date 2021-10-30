c005fb559bfe3aa1e68cfc5d725238c89be6f09e
PaulB-H <51964537+PaulB-H@users.noreply.github.com>
2021-10-30 16:21:55 -0400

    wrap script in onload event so I can grab correct height of #main

--- script.js
+++ script.js
@@ -1,118 +1,120 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+window.onload = (e) => {
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
 
-let initialWindowWidth = window.innerWidth;
+  let initialWindowWidth = window.innerWidth;
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 
-const techArr = [
-  "HTML",
-  "CSS",
-  "JavaScript",
-  "Node.JS",
-  "Express",
-  "Socket.IO",
-  "JSON",
-  "API's",
-  "React",
-  "Canvas",
-  "VPS",
-  "Ubuntu",
-  "Git",
-  "CLI",
-  "SQL",
-  "NoSQL",
-  "VS Code",
-];
+  const techArr = [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "Node.JS",
+    "Express",
+    "Socket.IO",
+    "JSON",
+    "API's",
+    "React",
+    "Canvas",
+    "VPS",
+    "Ubuntu",
+    "Git",
+    "CLI",
+    "SQL",
+    "NoSQL",
+    "VS Code",
+  ];
 
-const colorArr = [
-  "#0048BA",
-  "#B0BF1A",
-  "#7CB9E8",
-  "#C0E8D5",
-  "#B284BE",
-  "#72A0C1",
-  "#EDEAE0",
-  "#F0F8FF",
-  "#C46210",
-  "#EFDECD",
-  "#E52B50",
-  "#9F2B68",
-  "#3B7A57",
-  "#9966CC",
-];
+  const colorArr = [
+    "#0048BA",
+    "#B0BF1A",
+    "#7CB9E8",
+    "#C0E8D5",
+    "#B284BE",
+    "#72A0C1",
+    "#EDEAE0",
+    "#F0F8FF",
+    "#C46210",
+    "#EFDECD",
+    "#E52B50",
+    "#9F2B68",
+    "#3B7A57",
+    "#9966CC",
+  ];
 
-const fontArr = [
-  "Audiowide",
-  "Black Ops One",
-  "Bungee",
-  "Bungee Shade",
-  "Comfortaa",
-  "Fira Code",
-  "Gravitas One",
-  "Monoton",
-  "Rye",
-  "Special Elite",
-];
+  const fontArr = [
+    "Audiowide",
+    "Black Ops One",
+    "Bungee",
+    "Bungee Shade",
+    "Comfortaa",
+    "Fira Code",
+    "Gravitas One",
+    "Monoton",
+    "Rye",
+    "Special Elite",
+  ];
 
-let shadeInterv, clearInterv, drawInterv;
+  let shadeInterv, clearInterv, drawInterv;
 
-const drawIntervFunc = () => {
-  ctx.textBaseline = "top";
-  ctx.textAlign = "left";
+  const drawIntervFunc = () => {
+    ctx.textBaseline = "top";
+    ctx.textAlign = "left";
 
-  drawInterv = window.setInterval(() => {
-    const drawx = Math.ceil(Math.random() * canvas.width);
-    const drawy = Math.ceil(Math.random() * canvas.height);
+    drawInterv = window.setInterval(() => {
+      const drawx = Math.ceil(Math.random() * canvas.width);
+      const drawy = Math.ceil(Math.random() * canvas.height);
 
-    const randomTech = techArr[Math.floor(Math.random() * techArr.length)];
-    const randomFont = fontArr[Math.floor(Math.random() * fontArr.length)];
-    const randomColor = colorArr[Math.floor(Math.random() * colorArr.length)];
+      const randomTech = techArr[Math.floor(Math.random() * techArr.length)];
+      const randomFont = fontArr[Math.floor(Math.random() * fontArr.length)];
+      const randomColor = colorArr[Math.floor(Math.random() * colorArr.length)];
 
-    ctx.font = `30px ${randomFont}`;
-    ctx.fillStyle = randomColor;
+      ctx.font = `30px ${randomFont}`;
+      ctx.fillStyle = randomColor;
 
-    ctx.fillText(randomTech, drawx, drawy);
-  }, 50);
+      ctx.fillText(randomTech, drawx, drawy);
+    }, 50);
+  };
+  drawIntervFunc();
+
+  const shadeIntervFunc = () => {
+    shadeInterv = window.setInterval(() => {
+      ctx.fillStyle = "rgba(0,0,0,0.025)";
+
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }, 50);
+  };
+  shadeIntervFunc();
+
+  const clearIntervFunc = () => {
+    clearInterv = window.setInterval(() => {
+      window.clearInterval(drawInterv);
+      window.clearInterval(shadeInterv);
+    }, 3000);
+  };
+  clearIntervFunc();
+
+  window.addEventListener("resize", () => {
+    // Check if window actually resized
+    // On mobile scrolling can trigger this event
+
+    if (initialWindowWidth !== window.innerWidth) {
+      // console.log("resize triggered, resizing canvas");
+
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+
+      initialWindowWidth = window.innerWidth;
+
+      window.clearInterval(drawInterv);
+      window.clearInterval(clearInterv);
+      window.clearInterval(shadeInterv);
+
+      drawIntervFunc();
+      shadeIntervFunc();
+      clearIntervFunc();
+    }
+  });
 };
-drawIntervFunc();
-
-const shadeIntervFunc = () => {
-  shadeInterv = window.setInterval(() => {
-    ctx.fillStyle = "rgba(0,0,0,0.025)";
-
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-  }, 50);
-};
-shadeIntervFunc();
-
-const clearIntervFunc = () => {
-  clearInterv = window.setInterval(() => {
-    window.clearInterval(drawInterv);
-    window.clearInterval(shadeInterv);
-  }, 3000);
-};
-clearIntervFunc();
-
-window.addEventListener("resize", () => {
-  // Check if window actually resized
-  // On mobile scrolling can trigger this event
-
-  if (initialWindowWidth !== window.innerWidth) {
-    // console.log("resize triggered, resizing canvas");
-
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-
-    initialWindowWidth = window.innerWidth;
-
-    window.clearInterval(drawInterv);
-    window.clearInterval(clearInterv);
-    window.clearInterval(shadeInterv);
-
-    drawIntervFunc();
-    shadeIntervFunc();
-    clearIntervFunc();
-  }
-});
